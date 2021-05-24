edbce139d840b141a2b0ef10008e6196544ab606
Sean Shlimak <seanshlimak@gmail.com>
2021-05-24 18:07:57 +0300

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Sean Shlimak on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
